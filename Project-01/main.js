a8c37d6560177da36ed7f8e123756464f5c67be5
Latoya Watson <latoyamwatson@gmail.com>
2016-09-19 13:48:32 -0400

    added some functions

--- Project-01/main.js
+++ Project-01/main.js
@@ -1,33 +1,55 @@
 var alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
-
-// if letter is in the array, do this
-// if not
+var word = $("#enterWord #word-field");
 
 function getWord() {
-  var word = $("#guessWord #letter-field").val();
-  var letters = word.split(' ');
-  for (i = 0; i < letters.length; i++) {
-
-  }
+  word.val();
+  var letter = word.split(' ');
+    for (i = 0; i < letter.length; i++) {
+      $("#hangmanPlatform").append("<div class='dashes'></div>");
+    }
 }
 
 
-function translate (word) {
-    // take a string and make it into an array of letters
-  return word.split("")
-    // iterate over each letter in the array
-      .map(function (letter) {
-      // if the letter is a vowel
-      if( vowels.includes(letter) ) {
-          // add ub infront
-        return "ub" + letter
-      } else {
-          // otherwise just need the letter
-        return letter
-      }
-    })
-    // don't forget to turn the array back into a string
-    .join("")
-}
 
-translate("hello")
+// prevent refreshing when button or letter is clicked
+$(".alpha").on("click", function(e) {
+  e.preventDefault();
+  console.log(e);
+});
+
+$("#submit-word").on("click", function(e) {
+  e.preventDefault();
+  console.log(e);
+});
+
+// function handleGuesses() {
+//   if ()
+// }
+//
+// $("#a").on("click", handleGuesses);
+
+// if letter is in the array, do this
+// if not, add to hangman
+
+
+
+
+// function translate (word) {
+//     // take a string and make it into an array of letters
+//   return word.split("")
+//     // iterate over each letter in the array
+//       .map(function (letter) {
+//       // if the letter is a vowel
+//       if( vowels.includes(letter) ) {
+//           // add ub infront
+//         return "ub" + letter
+//       } else {
+//           // otherwise just need the letter
+//         return letter
+//       }
+//     })
+//     // don't forget to turn the array back into a string
+//     .join("")
+// }
+//
+// translate("hello")
