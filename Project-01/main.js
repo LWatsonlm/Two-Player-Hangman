8c88f057bfa46a6edcbf4cb64afd61a7026a9d72
Latoya Watson <latoyamwatson@gmail.com>
2016-09-20 15:44:31 -0400

    added logic for updating dashes

--- Project-01/main.js
+++ Project-01/main.js
@@ -7,47 +7,63 @@ var wordLength = 0;
 
 
 
-// var submit = $("#enterWord #submit-word");
+// this functions gets the typed in word, splits it and creates
+// dashes based on the length of the word
 
 function getWord(e) {
   e.preventDefault();
   secretWord = word.val();
-  letter = secretWord.split('');
-  var remainingLetters = secretWord.length;
-    for (i = 0; i < letter.length; i++) {
-      $(".dashes span").text(letter.length);
+  letters = secretWord.split('');
+  var remainingLetters = secretWord.length; // not used yet
+    for (i = 0; i < letters.length; i++) {
+      $(".dashes span").text(letters.length);
       answerArray[i] = " _ ";
       $(".dashes div").append(answerArray[i]);
-      console.log("why??");
+      console.log("word split works!");
     }
 }
 
-// if clicked #a ... #z
-// check if #a ... #z is in the secretWord array
-// if it's in the array, that's good
-// if it's not in the array, you get a strike (have 5 chances)
+// this function takes the letter from player two after they
+// click on the letter. It looks at the guess array and matches
+// it go the
 
-function guessLetter() {
-guess = $(this).attr('id');
-guessArray.push(guess);
-console.log(guess);
-var same = guessArray.every(function(element, index) {
-return element == secretWord[index];
+function letterGuess() {
+  guess = $(this).attr('id');
+  guessArray.push(guess);
+    console.log("guess: " + guess);
+  matches = letters.every(function(letter, index) {
+  return guessArray.includes(letter);
   });
-  console.log(same);
-  console.log(guessArray);
+    console.log("guessArray: " + guessArray);
+    console.log("matches: " + matches);
+    processGuess();
+}
+
+// p.code:
+// if letter is in array, update $(".dashes div") with letter
+// else
+// display hangman a body part of hangman
+
+function processGuess() {
+  // this is saying if all of the guess array is true, then fill out the letters. Which is why the letters are appearing after it true
+  if (guessArray.includes(letters) !== matches) {
+    $(".dashes div").html(letters);
+  } else {
+    alert("you got it wrong");
+  }
 }
 
 
 $("#enterWord #submit-word").on("click", getWord);
-$(".alpha").children().on("click", guessLetter);
+$(".alpha").children().on("click", letterGuess);
 // $(".alpha").children().on("click", css("background-color", red));
 
 
+// return letter == secretWord[index];
+// console.log("letter: " + letters);
+
 // if #a is in the letter array, display "you got a right letter"
 // else decrease remainingLetters by 1  example: remainingLetters--
-
-
 
 
 // prevent refreshing when button or letter is clicked
